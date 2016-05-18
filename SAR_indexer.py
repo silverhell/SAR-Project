8bf4cf6fef37caf3a7ea52037e995ad88d413ab4
Silverhell <d.picornell15@gmail.com>
2016-05-18 18:10:47 +0200

    MultiIndexer

--- SAR_indexer.py
+++ SAR_indexer.py
@@ -9,27 +9,33 @@ import os
 import pickle
 import sgml_parser as parser
 
-indice = {}
+#Indices y lista que contendra los indices al guardarlos en disco
+normalIndex = {}
+titleIndex = {}
+categoryIndex = {}
+dateIndex = {}
+finalIndex = []
+
 my_re = re.compile('\W+')
 
-def procesar_doc(doc):
+def procesar_doc(doc, eti):
     
     #Guardo como string el contenido del documento
     fitx = doc.read()
     #Deja las etiquetas en lineas separadas
     fitx = parser.parse(fitx)
     #Busqueda de los cuerpos de las noticias
-    textos = parser.busqueda('TEXT',fitx)
+    textos = parser.busqueda(eti,fitx)
     
     return textos
 
-def indexar(parsed_doc, docid):
+def indexar(parsed_doc, docid,indice):
     #Para cada noticia en el documento
     for i in range(len(parsed_doc)):
         aux = parsed_doc[i]
         #Paso el texto a minuscula
         aux = aux.lower()
-        #Elimino caracteres alfanumericos
+        #Elimino caracteres no alfanumericos
         aux = my_re.sub(' ',aux)
         #Transformamos en una lista de terminos
         aux = aux.split()
@@ -57,11 +63,33 @@ if __name__ == '__main__':
     index = sys.argv[2]
     fitxers = os.listdir(ruta)
     docid = 0
-    #Recorro los ficheros y creo el id del documento
+    #Recorro los ficheros buscando en los cuerpos de las noticias e indexando en su correspondiente indice
     for fitxer in fitxers:
         doc = open(ruta+'/'+fitxer, 'r')
-        pars_doc = procesar_doc(doc)
+        text_doc = procesar_doc(doc, 'TEXT')
         docid = docid+1
-        indexar(pars_doc,docid)
-    
-    pickle.dump(indice, open(index,'wb'))
+        indexar(text_doc,docid,normalIndex)
+
+    #Recorro los ficheros buscando en los titulos e indexando en su correspondiente indice
+    docid = 0
+    for fitxer in fitxers:
+        doc = open(ruta + '/' + fitxer, 'r')
+        title_doc = procesar_doc(doc, 'TITLE')
+        docid = docid + 1
+        indexar(title_doc, docid, titleIndex)
+
+    #Recorro los ficheros buscando en la etiqueta de categoria e indexando en su correspondiente indice
+    docid = 0
+    for fitxer in fitxers:
+        doc = open(ruta + '/' + fitxer, 'r')
+        cat_doc = procesar_doc(doc, 'CATEGORY')
+        docid = docid + 1
+        indexar(cat_doc, docid, categoryIndex)
+
+    #Añado a una lista final todos los indices para guardarlos en disco
+    finalIndex.append(normalIndex)
+    finalIndex.append(titleIndex)
+    finalIndex.append(categoryIndex)
+
+    #Guardo el archivo en disco con el nombre que se le ha proporcionado por consola
+    pickle.dump(finalIndex, open(index,'wb'))
